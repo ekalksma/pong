a4a29cfe1c218c890388d3ff9c9ae6605e3273a8
Emiel Kalksma <emiel.kalksma@gmail.com>
2020-04-05 17:56:55 +0200

    Add canvas

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 class Game {
   constructor() {
+    this.canvas = document.getElementById('canvas');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.canvas.width = 500;
+    this.canvas.height = 300;
+
     this.paddle = new Paddle();
   }
 
@@ -8,6 +14,16 @@ class Game {
   }
 
   loop() {
+    this.draw();
+
     window.requestAnimationFrame(this.loop.bind(this));
+  }
+
+  draw () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(200, 100);
+    this.ctx.stroke();
   }
 }
