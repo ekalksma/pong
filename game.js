78796b5ecf28dc0dc3fe85385d84509889270af0
Emiel Kalksma <emiel.kalksma@gmail.com>
2020-04-05 20:58:28 +0200

    Added player controls

--- game.js
+++ game.js
@@ -5,9 +5,11 @@ class Game {
 
     this.canvas.width = 500;
     this.canvas.height = 300;
+    const playerWidth = 10;
+    const playerHeight = 50;
 
-    this.player1 = new Paddle({ x: 30, y: 30 }, { w: 50, h: 10 });
-    this.player2 = new Paddle({ x: this.canvas.width - 30, y: 30 }, { w: 50, h: 10 });
+    this.player1 = new Paddle({ x: playerWidth / 2, y: this.canvas.height / 2 }, { w: playerWidth, h: playerHeight }, this.canvas.height, 68, 65);
+    this.player2 = new Paddle({ x: this.canvas.width - (playerWidth / 2), y: this.canvas.height / 2 }, { w: playerWidth, h: playerHeight },this.canvas.height, 37, 39);
   }
 
   run() {
@@ -15,15 +17,21 @@ class Game {
   }
 
   loop() {
-    this.draw();
+    this.update();
+    this.draw(this.ctx);
 
     window.requestAnimationFrame(this.loop.bind(this));
   }
 
-  draw () {
+  update() {
+    this.player1.update();
+    this.player2.update();
+  }
+
+  draw(ctx) {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.player1.draw(this.ctx);
-    this.player2.draw(this.ctx);
+    this.player1.draw(ctx);
+    this.player2.draw(ctx);
   }
 }
--- paddle.js
+++ paddle.js
@@ -1,8 +1,18 @@
 class Paddle {
-  constructor(position, size) {
+  constructor(position, size, canvasHeight, keyCodeLeft, keyCodeRight) {
     this.position = position;
     this.size = size;
+    const maxBoundary = canvasHeight;
     this.color = '#FF0000';
+    this.speed = 3;
+    this.boundary = {min: 0, max: maxBoundary}
+    this.keyCodeLeft = keyCodeLeft;
+    this.keyCodeRight = keyCodeRight;
+    this.keyLeft = false;
+    this.keyRight = false;
+
+    window.addEventListener('keydown', this.onKeyDown.bind(this));
+    window.addEventListener('keyup', this.onKeyUp.bind(this));
   }
 
   draw(ctx) {
@@ -10,5 +20,30 @@ class Paddle {
     ctx.fillRect(this.position.x - this.size.w / 2,
                  this.position.y - this.size.h / 2,
                  this.size.w, this.size.h);
+  }
+
+  update() {
+    this.calcPosition();
+  }
+
+  calcPosition() {
+    if(this.keyLeft){
+      this.position.y += this.speed;
+      if(this.position.y + this.size.h / 2 > this.boundary.max) this.position.y = this.boundary.max - this.size.h / 2;
+    }
+    if(this.keyRight){
+      this.position.y -= this.speed;
+      if(this.position.y - this.size.h / 2 < this.boundary.min) this.position.y = this.boundary.min + this.size.h / 2;
+    }
+  }
+
+  onKeyDown(event) {
+    if (event.keyCode === this.keyCodeLeft) this.keyLeft = true;
+        else if (event.keyCode === this.keyCodeRight) this.keyRight = true;
+  }
+
+  onKeyUp(event) {
+    if (event.keyCode === this.keyCodeLeft) this.keyLeft = false;
+        else if (event.keyCode === this.keyCodeRight) this.keyRight = false;
   }
 }
