84c0a854d93fbc9e99d8b3af85dc793020545a86
Emiel Kalksma <emiel.kalksma@gmail.com>
2020-04-06 23:49:56 +0200

    Refactor paddle / wall collision logic

--- game.js
+++ game.js
@@ -2,14 +2,17 @@ class Game {
   constructor() {
     this.canvas = document.getElementById('canvas');
     this.ctx = this.canvas.getContext('2d');
-
     this.canvas.width = 500;
     this.canvas.height = 300;
+    this.keyLeft = false;
+    this.keyRight = false;
     const playerWidth = 10;
     const playerHeight = 50;
 
-    this.player1 = new Paddle({ x: playerWidth / 2, y: this.canvas.height / 2 }, { w: playerWidth, h: playerHeight }, this.canvas.height, 68, 65);
-    this.player2 = new Paddle({ x: this.canvas.width - (playerWidth / 2), y: this.canvas.height / 2 }, { w: playerWidth, h: playerHeight },this.canvas.height, 37, 39);
+    this.player = new Paddle({ x: playerWidth / 2, y: this.canvas.height / 2 }, { w: playerWidth, h: playerHeight });
+
+    window.addEventListener('keydown', this.onKeyDown.bind(this));
+    window.addEventListener('keyup', this.onKeyUp.bind(this));
   }
 
   run() {
@@ -24,14 +27,33 @@ class Game {
   }
 
   update() {
-    this.player1.update();
-    this.player2.update();
+    if (this.keyLeft) this.player.moveUp();
+    if (this.keyRight) this.player.moveDown();
+
+    if (!this.isInBounds(this.player)) {
+      this.player.handleOutOfBounds(this.canvas);
+    }
   }
 
   draw(ctx) {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.player1.draw(ctx);
-    this.player2.draw(ctx);
+    this.player.draw(ctx);
+  }
+
+  onKeyDown(event) {
+    if (event.keyCode === 65) this.keyLeft = true;
+        else if (event.keyCode === 68) this.keyRight = true;
+  }
+
+  onKeyUp(event) {
+    if (event.keyCode === 65) this.keyLeft = false;
+        else if (event.keyCode === 68) this.keyRight = false;
+  }
+
+  isInBounds(player) {
+    const pos = this.player.position;
+    const size = this.player.size;
+    return pos.y + size.h / 2 < this.canvas.height && pos.y - size.h / 2 > 0;
   }
 }
--- paddle.js
+++ paddle.js
@@ -1,18 +1,9 @@
 class Paddle {
-  constructor(position, size, canvasHeight, keyCodeLeft, keyCodeRight) {
+  constructor(position, size) {
     this.position = position;
     this.size = size;
-    const maxBoundary = canvasHeight;
     this.color = '#FF0000';
     this.speed = 3;
-    this.boundary = {min: 0, max: maxBoundary}
-    this.keyCodeLeft = keyCodeLeft;
-    this.keyCodeRight = keyCodeRight;
-    this.keyLeft = false;
-    this.keyRight = false;
-
-    window.addEventListener('keydown', this.onKeyDown.bind(this));
-    window.addEventListener('keyup', this.onKeyUp.bind(this));
   }
 
   draw(ctx) {
@@ -23,27 +14,18 @@ class Paddle {
   }
 
   update() {
-    this.calcPosition();
   }
 
-  calcPosition() {
-    if(this.keyLeft){
-      this.position.y += this.speed;
-      if(this.position.y + this.size.h / 2 > this.boundary.max) this.position.y = this.boundary.max - this.size.h / 2;
-    }
-    if(this.keyRight){
-      this.position.y -= this.speed;
-      if(this.position.y - this.size.h / 2 < this.boundary.min) this.position.y = this.boundary.min + this.size.h / 2;
-    }
+  handleOutOfBounds(canvas) {
+    if (this.position.y + this.size.h / 2 > canvas.height) this.position.y = this.size.h / 2;
+     else if (this.position.y - this.size.h / 2 < 0) this.position.y = canvas.height - this.size.h / 2;
   }
 
-  onKeyDown(event) {
-    if (event.keyCode === this.keyCodeLeft) this.keyLeft = true;
-        else if (event.keyCode === this.keyCodeRight) this.keyRight = true;
+  moveUp() {
+    this.position.y -= this.speed;
   }
 
-  onKeyUp(event) {
-    if (event.keyCode === this.keyCodeLeft) this.keyLeft = false;
-        else if (event.keyCode === this.keyCodeRight) this.keyRight = false;
+  moveDown() {
+    this.position.y += this.speed;
   }
 }
